f800284fae7b436282a4bc378388f9b15b25f784
Richard <71474562+RichardOlaniyan@users.noreply.github.com>
2025-03-11 23:20:31 -0500

    Update main.py

--- Random_Pwd_Gen/main.py
+++ Random_Pwd_Gen/main.py
@@ -6,13 +6,13 @@ alpha = list(string.ascii_letters)
 numbers = list(string.digits)
 special_characters = list(string.punctuation)
 pwd_complexity = ""
-no_of_chracters = 0
 
-no_of_chracters = int(input("Length of password? \n"))
-while no_of_chracters <8:
+
+no_of_characters = int(input("Length of password? \n"))
+while no_of_characters <8:
     print("Minimum of 8 characters is needed for a strong password")
-    no_of_chracters = int(input("Length of password? \n"))
-    if no_of_chracters >= 8:
+    no_of_characters = int(input("Length of password? \n"))
+    if no_of_characters >= 8:
         break
 
 pwd_complexity = str(input("Complexity of password? (a = High security (recommended for financial accounts), b = Standard security (suitable for general websites)) \n"))
@@ -24,26 +24,26 @@ while pwd_complexity not in ("a", "b"):
 
 if pwd_complexity == "a":
     your_new_pwd = []
-    for _ in range(no_of_chracters):
+    for _ in range(no_of_characters):
         your_new_pwd.append(secrets.choice(alpha))
-    for _ in range(no_of_chracters):
+    for _ in range(no_of_characters):
         your_new_pwd.append(secrets.choice(numbers))
-    for _ in range(no_of_chracters):
+    for _ in range(no_of_characters):
         your_new_pwd.append(secrets.choice(special_characters))
-    pwd = "".join(secrets.choice(your_new_pwd) for i in range(no_of_chracters))
+    pwd = "".join(secrets.choice(your_new_pwd) for i in range(no_of_characters))
     print("--------------------------********************------------------")    
     print(f"Your secure password is: {pwd}")
     print("--------------------------********************------------------")  
     print("This password was generated using cryptographic-quality randomness for sensitive applications. Consider using it for Banking, email, primary accounts")
 elif pwd_complexity == "b":
     your_new_pwd = []
-    for _ in range(no_of_chracters):
+    for _ in range(no_of_characters):
         your_new_pwd.append(random.choice(alpha))
-    for _ in range(no_of_chracters):
+    for _ in range(no_of_characters):
         your_new_pwd.append(random.choice(numbers))
-    for _ in range(no_of_chracters):
+    for _ in range(no_of_characters):
         your_new_pwd.append(random.choice(special_characters))
-    pwd = "".join(random.choice(your_new_pwd) for i in range(no_of_chracters))
+    pwd = "".join(random.choice(your_new_pwd) for i in range(no_of_characters))
     print("--------------------------********************------------------")
     print(f"Your insecure password is: {pwd}")
     print("--------------------------********************------------------")  
